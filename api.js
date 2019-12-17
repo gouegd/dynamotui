160ab53342ab3305b54a660354ee8ad8d903aab5
Gregory Desfour <4745060-gouegd@users.noreply.gitlab.com>
2019-12-17 22:59:07 +1300

    Lint, version, remove fake test

--- api.js
+++ api.js
@@ -5,34 +5,34 @@ const shell = require('shelljs');
 const { useState, useEffect } = React;
 
 const callDynamo = command =>
-	new Promise((resolve, reject) =>
-		shell.exec(
-			`aws dynamodb ${command} --output json`,
-			{ silent: true },
-			(code, stdout, stderr) => {
-				if (code) {
-					reject(stderr);
-				} else resolve(JSON.parse(stdout));
-			}
-		)
-	);
+  new Promise((resolve, reject) =>
+    shell.exec(
+      `aws dynamodb ${command} --output json`,
+      { silent: true },
+      (code, stdout, stderr) => {
+        if (code) {
+          reject(stderr);
+        } else resolve(JSON.parse(stdout));
+      }
+    )
+  );
 
 const useDynamo = (command, { skip } = {}) => {
-	const [{ loading, error, data }, setResult] = useState({ loading: true });
+  const [{ loading, error, data }, setResult] = useState({ loading: true });
 
-	useEffect(() => {
-		if (!skip) {
-			setResult({ loading: true });
-			callDynamo(command)
-				.then(data => setResult({ data }))
-				.catch(error_ => setResult({ error: error_ }));
-		}
-	}, [command, skip]);
+  useEffect(() => {
+    if (!skip) {
+      setResult({ loading: true });
+      callDynamo(command)
+        .then(data => setResult({ data }))
+        .catch(error_ => setResult({ error: error_ }));
+    }
+  }, [command, skip]);
 
-	return { loading, error, data };
+  return { loading, error, data };
 };
 
 module.exports = {
-	useDynamo,
-	callDynamo,
+  useDynamo,
+  callDynamo,
 };
